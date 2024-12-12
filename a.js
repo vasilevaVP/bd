d2d240cc27bb60c20c14c17a3f8048f2334feaf1
vasilevaVP <vasileva.vp1455@mail.ru>
2024-12-12 17:13:15 +0500

    изменено

--- a.js
+++ a.js
@@ -32,6 +32,37 @@ async function run() {
     await sequelize.sync({ force: true });
     console.log("Таблица User пересоздана.");
 
+    //  // Создание пользователей
+    //  await User.create({ name: "John Doe", age: 30, cash: 100 });
+    //  await User.create({ name: "Jane Smith", age: 25, cash: 200 });
+    //  await User.create({ name: "Oleg", age: 40, cash: 1000 });
+    //  await User.create({ name: "Natasha", age: 19, cash: 500 });
+
+    //  // Основы выполнения запросов
+
+    //  // 1. Создание экземпляра
+    //  const john = await User.create({ name: "John Smith", age: 35, cash: 50 });
+    //  console.log("Создан пользователь John:", john.toJSON());
+
+    //  // 2. Создание экземпляра с определенными полями
+    //  const userWithUsername = await User.create(
+    //    { name: "UserWithUsernameOnly", age: 28, cash: 300 },
+    //    { fields: ["name"] }
+    //  ); // Сохранятся только поле name
+    //  console.log("Создан пользователь с указанными полями:", userWithUsername.toJSON());
+
+    //  // 3. Получение экземпляра
+    //  const firstUser = await User.findOne(); // Получает первый найденный экземпляр
+    //  console.log("Первый пользователь:", firstUser ? firstUser.toJSON() : "Нет пользователей");
+
+    //  // 4. Получение всех пользователей
+    //  const allUsers = await User.findAll();
+    //  console.log("Все пользователи:", allUsers.map((user) => user.toJSON()));
+
+    //  // 5. Получение пользователей по условию
+    //  const usersOver30 = await User.findAll({ where: { age: { [Op.gt]: 30 } } });
+    //  console.log("Пользователи старше 30:", usersOver30.map((user) => user.toJSON()));
+
     // // Создание нескольких экземпляров одновременно
     // const users = await User.bulkCreate([
     //   { name: "TTT", age: 30, cash: 100 },
